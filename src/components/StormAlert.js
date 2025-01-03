a33e38158de41a7450bd27e9515018ea0b52f90a
aaishahhulkhoree <hulkhoreea@gmail.com>
2025-01-03 14:11:51 +0100

    changement de storm pour evaluer par rapport a windSpeed et plus windGust

--- src/components/StormAlert.js
+++ src/components/StormAlert.js
@@ -2,11 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types'; // Import de PropTypes
 import '../assets/styles/alert.css'
 
-
-const StormAlert = ({ windSpeed, windGust, forecastWindSpeed }) => {
-  // Condition pour un vent violent
+const StormAlert = ({ windSpeed, forecastWindSpeed }) => {
+  // Condition pour un vent violent ou rafales fortes
   const isWindStrong = windSpeed > 30; // Vent régulier > 30 km/h
-  const isWindGustStrong = windGust > 60; // Rafales de vent > 60 km/h
+  const isWindGustStrong = windSpeed > 60; // Rafales de vent > 60 km/h
   const isSevereWindForecast = forecastWindSpeed > 50; // Prévision de vent supérieur à 50 km/h
   
   if (isWindStrong || isWindGustStrong || isSevereWindForecast) {
@@ -15,8 +14,8 @@ const StormAlert = ({ windSpeed, windGust, forecastWindSpeed }) => {
         <h3>Alerte Tempête !</h3>
         <p>
           {isWindGustStrong 
-            ? `Rafales de vent violentes détectées avec une vitesse de ${windGust} km/h.`
-            : `Vent violent détecté avec une vitesse de ${windSpeed} km/h.`}
+            ? `Rafales de vent violentes détectées avec une vitesse de ${Math.round(windSpeed)} km/h.`
+            : `Vent violent détecté avec une vitesse de ${Math.round(windSpeed)} km/h.`}
         </p>
         {isSevereWindForecast && <p>Prévisions : vent supérieur à 50 km/h dans les prochaines heures.</p>}
       </div>
@@ -29,7 +28,6 @@ const StormAlert = ({ windSpeed, windGust, forecastWindSpeed }) => {
 // Définir les PropTypes pour valider les props du composant StormAlert
 StormAlert.propTypes = {
   windSpeed: PropTypes.number.isRequired, // windSpeed doit être un nombre et est requis
-  windGust: PropTypes.number.isRequired, // windGust doit être un nombre et est requis
   forecastWindSpeed: PropTypes.number.isRequired, // forecastWindSpeed doit être un nombre et est requis
 };
 
